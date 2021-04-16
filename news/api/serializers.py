7c9075a101cc8669df9a4f7c20b97926c30296cc
mohaiminul94 <rabidislam94@gmail.com>
2021-04-17 02:34:05 +0600

    Serializer validation added

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -17,3 +17,13 @@ class ArticleSerializer(serializers.ModelSerializer):
         now= datetime.now()
         time_difference= timesince(publication_date, now)
         return time_difference     
+
+    def validate(self, data):
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError("Title and Description must be different from one another!")
+        return data
+
+    def validate_title(self, value):
+        if len(value) < 60:
+            raise serializers.ValidationError("The title has to be at least 60 chars long!")
+        return value
